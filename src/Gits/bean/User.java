78132c93d5452c72668126f0bcd8aa219679763e
Root <1138841489@qq.com>
2020-11-14 16:14:00 +0800

    第一次提交User

---
+++ src/Gits/bean/User.java
@@ -0,0 +1,18 @@
+package Gits.bean;
+
+/**
+ * @ProjectName: github
+ * @Package: Gits.bean
+ * @Description: java类作用描述
+ * @Author: 王钧烽
+ * @CreateDate: 2020/11/14 16:07
+ * @Version: 1.0
+ * <p>
+ * Copyright: Copyright (c) 2020
+ */
+
+public class User {
+    private String username;
+    private String password;
+
+}
